717328fe08793698ab8c7331f7b8fedf12174423
duho Kim <dev.kimduho@gmail.com>
2022-06-09 21:59:40 +0900

    Add #set_user to base_controller for api

--- app/controllers/api/v1/github/base_controller.rb
+++ app/controllers/api/v1/github/base_controller.rb
@@ -2,14 +2,18 @@ module Api
   module V1
     module Github
       class BaseController < Api::V1::ApiController
+        before_action :set_user
         before_action :set_client
 
         private
 
-        attr_reader :client
+        attr_reader :client, :user
+
+        def set_user
+          @user = ::Github::User.find(params[:id])
+        end
 
         def set_client
-          user = ::Github::User.find(params[:id])
           @client = ::Github::Client.new(user.auth_token)
         end
       end
