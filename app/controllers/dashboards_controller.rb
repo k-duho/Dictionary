fd08f13122a18752700188af259eae24f2af7d29
duho Kim <dev.kimduho@gmail.com>
2022-06-21 23:38:36 +0900

    Add a method to get login user's repositories

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,20 @@
 class DashboardsController < ApplicationController
   def index
-    @dashboard_info = fetch_dashboard_info
+    @github_user = fetch_github_user
+    @repositories = fetch_github_user_repos
   end
 
   private
 
-  def fetch_dashboard_info
-    
+  def github_client
+    @github_client ||= ::Github::Client.new(current_github_user.auth_token)
+  end
+
+  def fetch_github_user
+    github_client.get("/users/#{current_github_user.github_name}")
+  end
+
+  def fetch_github_user_repos
+    github_client.get("/users/#{current_github_user.github_name}/repos")
   end
 end
