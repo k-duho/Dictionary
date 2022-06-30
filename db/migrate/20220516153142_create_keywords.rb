fc1f60ed0e4d0d58dfb2b5f149b76f5d5afc2457
duho Kim <dev.kimduho@gmail.com>
2022-06-30 23:47:53 +0900

    Delete db/migrate/*

--- db/migrate/20220516153142_create_keywords.rb
+++
@@ -1,8 +0,0 @@
-class CreateKeywords < ActiveRecord::Migration[6.1]
-  def change
-    create_table :keywords do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20220517132612_board_hash_tags.rb
+++
@@ -1,9 +0,0 @@
-class BoardHashTags < ActiveRecord::Migration[6.1]
-  def change
-    create_table :board_hash_tags do |t|
-      t.integer :board_id
-      t.integer :keyword_id
-      t.timestamps
-    end
-  end
-end
