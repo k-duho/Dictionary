3908b6db4f3500a9c50da2831df498d5a3abd2e0
duho Kim <dev.kimduho@gmail.com>
2022-07-05 23:41:46 +0900

    깃헙 동기화 처리 추가(리포지토리)

---
+++ batches/sync_github_batch.rb
@@ -0,0 +1,33 @@
+class SyncGithubBatch
+  class << self
+    def run
+      new.exec
+    end
+  end
+
+  def initialize
+
+  end
+
+  def exec
+    GithubUser.all.each do |github_user|
+      github_client = init_client(github_user.auth_token)
+
+      sync_github_repository(github_user, github_client)
+    end
+  end
+
+  private
+
+  def sync_github_repository(github_user, github_client)
+    SyncRepositoryJob.perform_now(github_user, github_client)
+  end
+
+  def sync_github_organization(github_user, github_client)
+    SyncOrganizationJob.perform_now(github_user, github_client)
+  end
+
+  def init_client(github_user_token)
+    ::Github::Client.new(github_user_token)
+  end
+end
